5f1d40db111055255f07fa17d0fe7fc848d083a5
Raj Chaurasiya <rc808129@gmail.com>
2025-11-14 09:17:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,10 +13,18 @@ const getSum = () => {
     newRow.appendChild(firstCell);
     
     const secondCell = document.createElement('td');
-    secondCell.textContent = getSum(); // Yahan sum ko set karna
+    secondCell.textContent = sum; // Yahan sum ko set karna
     newRow.appendChild(secondCell);
     
     // Row ko table mein append karna
     const table = document.querySelector('table');
     table.appendChild(newRow);
 };
+
+// Button create karna
+const getSumBtn = document.createElement("button");
+getSumBtn.textContent = "Get Total Price"; // Button par text set karna
+document.body.appendChild(getSumBtn); // Button ko body mein add karna
+
+// Button click event listener
+getSumBtn.addEventListener("click", getSum); // Button par click hone par getSum function call karna
